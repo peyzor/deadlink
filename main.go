d7e5c79938273f506994e3c0a4a7c0782291bc9e
Peyman <hassani.peyman627@gmail.com>
2025-03-31 22:53:58 +0330

    Add printing html content of web page

--- main.go
+++ main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"io"
+	"net/http"
 	"os"
 )
 
@@ -11,6 +13,20 @@ func main() {
 		return
 	}
 
-	link := os.Args[1]
-	fmt.Println(link)
+	url := os.Args[1]
+
+	resp, err := http.Get(url)
+	if err != nil {
+		fmt.Printf("couldn't get url: %v", err)
+		return
+	}
+
+	defer resp.Body.Close()
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Printf("couldn't read url: %v", err)
+		return
+	}
+
+	fmt.Println(string(body))
 }
