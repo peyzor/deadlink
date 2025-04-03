473f8c2f6188d24971fe544f4c333c4e45f11dd1
Peyman <hassani.peyman627@gmail.com>
2025-04-03 16:39:02 +0330

    Add recursive mechanism to crawl page and extract all links

--- main.go
+++ main.go
@@ -5,9 +5,17 @@ import (
 	"fmt"
 	"golang.org/x/net/html"
 	"io"
+	"log"
+	"maps"
 	"net/http"
 	"os"
+	"slices"
+	"strings"
+	"time"
 )
+
+var visitedLinks = make(map[string]bool)
+var URL string
 
 func main() {
 	if len(os.Args) != 2 {
@@ -15,45 +23,85 @@ func main() {
 		return
 	}
 
-	url := os.Args[1]
+	URL = os.Args[1]
+	visitedLinks[URL] = true
 
-	resp, err := http.Get(url)
+	URL = strings.TrimSuffix(URL, "/")
+	err := crawlPage(URL)
 	if err != nil {
-		fmt.Printf("couldn't get url: %v", err)
-		return
+		log.Println(err)
 	}
 
+	links := slices.Collect(maps.Keys(visitedLinks))
+	fmt.Println(links)
+}
+
+func requestPage(url string) ([]byte, error) {
+	client := &http.Client{Timeout: 5 * time.Second}
+	resp, err := client.Get(url)
+	if err != nil {
+		fmt.Printf("couldn't get url: %v", err)
+		return nil, err
+	}
 	defer resp.Body.Close()
+
 	body, err := io.ReadAll(resp.Body)
 	if err != nil {
 		fmt.Printf("couldn't read url: %v", err)
-		return
+		return nil, err
 	}
 
+	return body, nil
+}
+
+func parseHTML(body []byte) (*html.Node, error) {
 	doc, err := html.Parse(bytes.NewReader(body))
 	if err != nil {
 		fmt.Printf("couldn't parse html: %v", err)
-		return
+		return nil, err
 	}
 
-	var allLinks []string
-	for n := range doc.Descendants() {
-		if n.Type == html.ElementNode && n.Data == "a" {
-			links := findLinks(n.Attr)
-			allLinks = append(allLinks, links...)
-		}
-	}
-
-	fmt.Println(allLinks)
+	return doc, nil
 }
 
-func findLinks(attrs []html.Attribute) []string {
-	var links []string
-	for _, attr := range attrs {
-		if attr.Key == "href" {
-			links = append(links, attr.Val)
+func findLinks(node *html.Node) {
+	for n := range node.Descendants() {
+		if n.Type != html.ElementNode || n.Data != "a" {
+			continue
+		}
+		for _, attr := range n.Attr {
+			if attr.Key != "href" {
+				continue
+			}
+			link := attr.Val
+			if _, ok := visitedLinks[link]; !ok {
+				visitedLinks[link] = true
+
+				if strings.HasPrefix(link, "http") {
+					continue
+				}
+
+				link = URL + link
+				err := crawlPage(link)
+				if err != nil {
+					log.Println(err)
+				}
+			}
 		}
 	}
+}
 
-	return links
+func crawlPage(url string) error {
+	body, err := requestPage(url)
+	if err != nil {
+		return err
+	}
+
+	node, err := parseHTML(body)
+	if err != nil {
+		return err
+	}
+
+	findLinks(node)
+	return nil
 }
